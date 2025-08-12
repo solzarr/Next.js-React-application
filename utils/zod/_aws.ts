fcca675b91fc0b727f5e730b80d35a9aecb6b08b
solzarr <marcustorvalds.se@gmail.com>
2025-08-12 18:28:47 +0800

    update docker file

--- utils/zod/_aws.ts
+++ utils/zod/_aws.ts
@@ -2,10 +2,13 @@ import z from 'zod';
 
 export const zodS3Upload = z.object({
   type: z.string(),
-  file: z.instanceof(File),
+  file: z.custom<File>((val) => {
+    if (typeof File === 'undefined') return true;
+    return val instanceof File;
+  }, 'Must be a File object'),
   fileExtension: z.string(),
   contentType: z.string(),
-  key: z.string(),
+  key: z.string().optional(),
   isPrivate: z.string().optional(),
   dimension: z.tuple([z.number(), z.number(), z.number()]).optional(),
 });
